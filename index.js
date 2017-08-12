a28fe14c5014b485f9aeffeea3ddb373b8800d2c
G100g <gigetto@gmail.com>
2017-08-12 11:41:27 +0000

    First button added

---
+++ index.js
@@ -0,0 +1,25 @@
+const Gpio = require("onoff").Gpio;
+const io = require('socket.io')(8080);
+
+const createButton = function (pin) {
+
+	const button = new Gpio(pin, 'in', 'both');
+	let lastValue = 0;
+	button.watch(function (err, value) {
+		if (lastValue !== value) {
+lastValue = value;
+		io.emit('button', {
+		'button1': value
+		})
+    		console.log(value)
+
+}
+})
+
+	return button;
+
+}
+
+
+const button1 = createButton(18);
+
